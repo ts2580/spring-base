0f42f6c00b5d659f91d396235506dd85243aedcf
trstyq <trstyq@gmail.com>
2021-10-29 10:03:09 +0900

    [성진] 테스트

---
+++ src/main/java/com/kh/spring/member/validator/TEST.java
@@ -0,0 +1,15 @@
+package com.kh.spring.member.validator;
+
+import java.sql.Date;
+
+public class TEST {
+	
+	private int no;
+	private String title;
+	private String content;
+	private String writer;
+	private String status;
+	private Date createDate;
+	private Date updateDate;
+
+}
